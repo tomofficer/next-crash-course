9203972b4d2092da0615a8eae573f06f5123bb17
Tom Officer <thomasofficerjr@gmail.com>
2021-11-16 23:35:25 -0800

    added article/id index file with routes, tested deployment to local 8000 in out folder

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,11 +1,11 @@
 import articleStyles from '../styles/Article.module.css'
-
+import ArticleItem from './ArticleItem'
 
 const ArticleList = ({ articles }) => {
     return (
         <div className={articleStyles.grid}>
             {articles.map((article) => (
-                <h3>{article.title}</h3>
+                <ArticleItem article={article} key="article"/>
             ))}
 
         </div>
