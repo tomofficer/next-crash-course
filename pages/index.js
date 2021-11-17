bec33d63b47c679b1c9a194516c1e8f245c17f33
Tom Officer <thomasofficerjr@gmail.com>
2021-11-16 23:09:51 -0800

    added ArticleList component and fetch call

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
+import ArticleList from '../components/ArticleList'
 
 
+export default function Home({ articles }) {
 
-export default function Home() {
   return (
     <div>
       <Head>
@@ -10,7 +11,20 @@ export default function Home() {
         <meta name='keywords' content='web development, programing'/>
       </Head>
 
-      <h1>Welcome to Next!</h1>
+      <ArticleList articles={articles}/>
     </div>
   )
+}
+
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
